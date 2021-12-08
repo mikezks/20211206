10b763c9726a4b8a423d4f222390c80b5b981656
michael.zikes <michael.zikes@intauria.com>
2021-12-08 10:07:08 +0100

    Redux: connect router state

--- apps/flight-app/src/app/+state/index.ts
+++ apps/flight-app/src/app/+state/index.ts
@@ -1,5 +1,7 @@
+import { getSelectors, routerReducer, RouterReducerState } from '@ngrx/router-store';
 import {
   ActionReducerMap,
+  createFeatureSelector,
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
@@ -10,8 +12,15 @@ export interface State {
 }
 
 export const reducers: ActionReducerMap<State> = {
-
+  router: routerReducer
 };
 
 
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+
+
+export const selectRouter = createFeatureSelector<RouterReducerState>('router');
+
+export const {
+  selectRouteParams
+} = getSelectors(selectRouter);
